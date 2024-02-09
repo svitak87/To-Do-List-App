8bbcf2577852404b508eb58987289d26b81023ef
svitak87 <svitakwe87@hotmail.com>
2024-02-09 13:11:34 -0300

    .env

--- server/database/db.js
+++ server/database/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "admin",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 module.exports = pool;
